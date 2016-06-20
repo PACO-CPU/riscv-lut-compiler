5adfbf5d1b27811c9d34904679c3afa4794e12ab
Peter Wagener <peterwagener@gmx.de>
2016-06-20 11:40:26 +0200

    added system test facilities

--- builddep.py
+++ builddep.py
@@ -7,13 +7,13 @@ CXXFLAGS=["-std=c++11", "-I."]
 
 
 objects=[]
+tests=[]
 for root,dirs,files in os.walk("."):
-  
-  for fn in files:
-    fn=os.path.join(root,fn)
+  for fn_base in files:
+    fn=os.path.join(root,fn_base)
     (fc,fe)=os.path.splitext(fn)
     fe=fe.lower()
-
+    
     if fe in { ".c", ".cpp", ".cc" }:
       obj="%s.o"%fc
       p=sp.Popen(
@@ -26,5 +26,10 @@ for root,dirs,files in os.walk("."):
         sys.exit(p.returncode)
       
       objects.append(obj)
+    elif fn.startswith("./tests/") and fn_base in { "run.sh" }:
+      ident="test-%s"%os.path.basename(root)
+      print("%s:\n\tcd %s && ./%s"%(ident,root,fn_base))
+      tests.append(ident)
 
 print("OBJ+=%s"%(" ".join(objects)))
+print("TESTS+=%s"%(" ".join(tests)))
