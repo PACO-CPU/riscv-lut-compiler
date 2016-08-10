7c5c12d307835772aa0162a8c7d8545d52894a9c
Peter Wagener <peterwagener@gmx.de>
2016-08-10 12:20:31 +0200

    fixed segment-uniform test case

--- tests/segment-uniform/run.sh
+++ tests/segment-uniform/run.sh
@@ -2,8 +2,8 @@
 
 echo -e "\x1b[34;1msegmentation test: uniform\x1b[30;0m"
 function runnamed() {
-  #../../riscv-lut-compiler -i --arch $1.arch $1.input
-  ../../riscv-lut-compiler --arch $1.arch $1.input
+  ../../riscv-lut-compiler -i --arch $1.arch $1.input
+  #../../riscv-lut-compiler --arch $1.arch $1.input
   colordiff $1.lut $1.lut.cmp
 }
 
